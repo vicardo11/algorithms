1b4db2452e0283a9c951c2cf89bdbe2073f93357
Patryk Sosiński <62787958+vicardo11@users.noreply.github.com>
2021-04-24 12:33:42 +0200

    Create IverseStrings.java

---
+++ strings/IverseStrings.java
@@ -0,0 +1,11 @@
+// Check if given Strings are inverse of each other
+
+public class IverseStrings {
+    public static boolean areInverseStrings(String text1, String text2) {
+        StringBuilder stringBuilder = new StringBuilder(text1);
+        StringBuilder reverseText1 = stringBuilder.reverse();
+        String reverseText1AsString = reverseText1.toString();
+
+        return reverseText1AsString.equals(text2);
+    }
+}
