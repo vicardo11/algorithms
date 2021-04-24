bd6976d804b4e60f6f156e67698250603f8d8333
Patryk Sosiński <62787958+vicardo11@users.noreply.github.com>
2021-04-24 11:52:45 +0200

    Create InsertionSort.java

---
+++ sort/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public static void sortArray(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
