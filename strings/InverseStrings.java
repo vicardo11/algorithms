d62aca2c87e2dfbae8fbb3a89a4fb4e4daaf62eb
Patryk Sosiński <62787958+vicardo11@users.noreply.github.com>
2021-04-24 13:12:32 +0200

    Update and rename IverseStrings.java to InverseStrings.java

--- strings/InverseStrings.java
+++ strings/InverseStrings.java
@@ -1,6 +1,6 @@
 // Check if given Strings are inverse of each other
 
-public class IverseStrings {
+public class InverseStrings {
     public static boolean areInverseStrings(String text1, String text2) {
         StringBuilder stringBuilder = new StringBuilder(text1);
         StringBuilder reverseText1 = stringBuilder.reverse();
